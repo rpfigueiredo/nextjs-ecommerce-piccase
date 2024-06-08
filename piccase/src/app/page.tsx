7d6d5d1a88247b041ab41a95929ddd9b068c8179
rpfigueiredo <rodrigo.prado.figueiredo@gmail.com>
2024-06-08 19:51:27 -0300

    Adiciona secao de envio de foto para capa personalizada

--- piccase/src/app/page.tsx
+++ piccase/src/app/page.tsx
@@ -2,8 +2,10 @@ import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Reviews } from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -114,7 +116,7 @@ export default function Home() {
               </span>{' '}
               dizem
             </h2>
-            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+            <img src='/review.png' className='w-24 order-0 lg:order-2' />
           </div>
 
           <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
@@ -164,9 +166,9 @@ export default function Home() {
                 <p>
                   "Normalmente, eu guardo meu celular junto com minhas chaves no bolso, e isso causou alguns arranhões bem pesados em todas as minhas últimas capas de celular. {' '}
                   <span className='p-0.5 bg-slate-800 text-white'>
-                  Parece como nova mesmo após cerca de seis meses de uso.
+                    Parece como nova mesmo após cerca de seis meses de uso.
                   </span>
-                  . Eu gosto muito dela."
+                  Eu gosto muito dela."
                 </p>
               </div>
               <div className='flex gap-4 mt-2'>
@@ -190,6 +192,70 @@ export default function Home() {
         <div className='pt-16'>
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className='py-24'>
+          <div className='mb-12 px-6 lg:px-8'>
+            <div className='mx-auto max-w-2xl sm:text-center'>
+              <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+                 Envie sua foto e tenha{' '}
+                <span className='relative px-2 bg-green-600 text-white'>
+                  sua própria case
+                </span>{' '}
+                única
+              </h2>
+            </div>
+          </div>
+
+          <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+            <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
+              <img
+                src='/arrow.png'
+                className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
+              />
+
+              <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
+                <img
+                  src='/horse.jpg'
+                  className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+                />
+              </div>
+
+              <Phone className='w-60' imgSrc='/horse_phone.jpg' />
+            </div>
+          </div>
+          <ul className='mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit'>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Material de silicone de alta qualidade
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Revestimento resistente a arranhões e impressões digitais
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Compatível com carregamento sem fio
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Garantia de impressão de 5 anos
+            </li>
+
+            <div className='flex justify-center'>
+              <Link
+                className={buttonVariants({
+                  size: 'lg',
+                  className: 'mx-auto mt-8',
+                })}
+                href='/configure/upload'>
+                Crie sua capa agora <ArrowRight className='h-4 w-4 ml-1.5' />
+              </Link>
+            </div>
+          </ul>
+
+        </MaxWidthWrapper>
       </section>
     </div>
   )
