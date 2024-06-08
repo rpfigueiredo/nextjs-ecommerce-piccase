a617d5582d8b16bd78e3f503462ef074cec0bc17
rpfigueiredo <rodrigo.prado.figueiredo@gmail.com>
2024-06-08 18:09:45 -0300

    Adiciona reviews dos usuarios e a secao de fotos dos reviews

--- piccase/src/app/page.tsx
+++ piccase/src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -98,6 +100,97 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
+
+
+      {/* value proposition section */}
+      <section className='bg-slate-100 grainy-dark py-24'>
+        <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
+          <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+              O que nossos{' '}
+              <span className='relative px-2'>
+                clientes{' '}
+                <Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500' />
+              </span>{' '}
+              dizem
+            </h2>
+            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+          </div>
+
+          <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  "A capa parece durável e até recebi um elogio pelo design. Estou com a capa há dois meses e meio e{' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                    a imagem está super nítida
+                  </span>
+                  , na capa que eu tinha antes, a imagem começou a desbotar para uma cor amarelada após algumas semanas. Adorei."
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-1.png'
+                  alt='usuário'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Jonathan</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Compra Verificada</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* second user review */}
+            <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+              <div className='flex gap-0.5 mb-2'>
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+                <Star className='h-5 w-5 text-green-600 fill-green-600' />
+              </div>
+              <div className='text-lg leading-8'>
+                <p>
+                  "Normalmente, eu guardo meu celular junto com minhas chaves no bolso, e isso causou alguns arranhões bem pesados em todas as minhas últimas capas de celular. {' '}
+                  <span className='p-0.5 bg-slate-800 text-white'>
+                  Parece como nova mesmo após cerca de seis meses de uso.
+                  </span>
+                  . Eu gosto muito dela."
+                </p>
+              </div>
+              <div className='flex gap-4 mt-2'>
+                <img
+                  className='rounded-full h-12 w-12 object-cover'
+                  src='/users/user-4.jpg'
+                  alt='user'
+                />
+                <div className='flex flex-col'>
+                  <p className='font-semibold'>Bruno</p>
+                  <div className='flex gap-1.5 items-center text-zinc-600'>
+                    <Check className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <p className='text-sm'>Compra Verificada</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className='pt-16'>
+          <Reviews />
+        </div>
+      </section>
     </div>
-  );
+  )
 }
