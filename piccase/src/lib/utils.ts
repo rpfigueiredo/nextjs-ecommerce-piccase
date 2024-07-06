2260261b818ba63d6b1a0ada3d7c0d11687090e1
rpfigueiredo <rodrigo.prado.figueiredo@gmail.com>
2024-07-06 02:49:21 -0300

    feat: implementa função para configurar metadados da página com informações de SEO.

--- piccase/src/lib/utils.ts
+++ piccase/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from 'next'
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +14,35 @@ export const formatPrice = (price: number) => {
   })
 
   return formatter.format(price)
+}
+
+export function constructMetadata({
+  title = 'PicCase - Cases personalizadas',
+  description = 'Crie cases personalizadas para seu celular em segundos',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@rpfigueired0',
+    },
+    icons,
+    metadataBase: new URL("https://piccase-dev.vercel.app/")
+  }
 }
