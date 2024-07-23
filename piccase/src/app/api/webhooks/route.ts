ccf73f3669d8531d6dc18f30136b3d66b5de456b
rpfigueiredo <rodrigo.prado.figueiredo@gmail.com>
2024-07-23 04:41:01 -0300

    fix: Corrige bugs

--- piccase/src/app/api/webhooks/route.ts
+++ piccase/src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
             })
 
             await resend.emails.send({
-                from: 'PicCase <rodrigo.figueiredo.prado@gmail.com>',
+                from: 'PicCase <rodrigo.prado.figueiredo@gmail.com>',
                 to: [event.data.object.customer_details.email],
                 subject: 'Obrigado pelo seu pedido!',
                 react: OrderReceivedEmail({
